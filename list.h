ef6e6ab7c3af2e0aaaed0fe94e748b39cc4bb935
Noa <noadragona@gmail.com>
2022-09-21 13:01:50 +0200

    dudas planteadas

--- list.h
+++ list.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define LNULL NULL
-#define MAX_LENGHT 100
+#define MAX_LENGHT 100 //este es el largo de una cadena con punteros a otras, debería ser tanto??
 
 typedef int posicion;
 typedef struct{
